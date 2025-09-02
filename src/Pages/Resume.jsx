094e4111338f070e13a0fbfa094a46f6ee2f4a79
wjollie <william.jollie23@gmail.com>
2025-09-02 16:25:36 -0400

    removed resume download

--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -7,7 +7,7 @@ export default function Resume() {
       <section className="hero">
         <div className="container resume-wrap">
           <h1>Resume</h1>
-          <p className="intro">One-page resume. Download resume PDF with the button below.</p>
+          {/* <p className="intro">One-page resume. Download resume PDF with the button below.</p>
 
           <a
   href="/public/resume.pdf"
@@ -15,8 +15,6 @@ export default function Resume() {
   className="Btn-Container resume-download"
 >
   <div className="text">Download PDF</div>
-
-  {/* Animated icon (same markup, same CSS) */}
   <span className="dl-icon" aria-hidden="true">
     <svg className="dl-svg" viewBox="0 0 384 512" xmlns="http://www.w3.org/2000/svg">
       <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/>
@@ -24,7 +22,7 @@ export default function Resume() {
     <span className="dl-bar" />
     <span className="dl-tooltip">Download</span>
   </span>
-</a>
+</a> */}
 
 
 
